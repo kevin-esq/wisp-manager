4c0aa577813bbfefa8076e8e2a036a1933c40e6a
Kevin Esquivel <dev.kevinesquivel@gmail.com>
2025-09-03 20:06:28 -0500

    feat(ui): add initial navigation skeleton with base pages
    - Added NavigationView with base menu items (Home, Clients, Plans, Inventory, Routers, Settings)
    - Configured ContentFrame for page navigation
    - Prepared structure for future features (Payments, etc.)

--- WispManager.UI/MainWindow.xaml.cs
+++ WispManager.UI/MainWindow.xaml.cs
@@ -1,31 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using WispManager.UI.Views;
 
 namespace WispManager.UI
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            NavView.ItemInvoked += OnItemInvoked;
+
+            NavView.SelectedItem = NavView.MenuItems[0];
+            ContentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+                return;
+            }
+
+            var tag = (args.InvokedItemContainer as NavigationViewItem)?.Tag?.ToString();
+            switch (tag)
+            {
+                case "home":
+                    ContentFrame.Navigate(typeof(HomePage)); break;
+                case "clients":
+                    ContentFrame.Navigate(typeof(ClientsPage)); break;
+                case "plans":
+                    ContentFrame.Navigate(typeof(PlansPage)); break;
+                case "inventory":
+                    ContentFrame.Navigate(typeof(InventoryPage)); break;
+                case "routers":
+                    ContentFrame.Navigate(typeof(RouterPage)); break;
+            }
         }
     }
 }
